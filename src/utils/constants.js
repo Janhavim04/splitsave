841c038a7379715f183c5cfc956177e6408afe7a
Janhavim04 <janhavimane123@gmail.com>
2026-04-20 14:46:33 +0530

    Add app constants configuration file

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+// SplitSave App Constants
+export const APP_NAME        = 'SplitSave'
+export const APP_VERSION     = '1.0.0'
+export const NETWORK         = 'testnet'
+export const HORIZON_URL     = 'https://horizon-testnet.stellar.org'
+export const RPC_URL         = 'https://soroban-testnet.stellar.org'
+export const CACHE_TTL       = 30 // seconds
+export const MAX_ACTIVITIES  = 20
+export const MIN_PAYMENT     = 0.0000001 // minimum XLM payment
+export const STROOP          = 10000000  // 1 XLM = 10,000,000 stroops
